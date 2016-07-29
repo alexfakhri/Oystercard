7c22ffeb21e21e4c1b5f357bff2f3273427230dc
alexfakhri <alexfakhri@me.com>
2016-07-29 13:40:49 +0100

    Added test and method for deducting amount from the balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,9 +7,9 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
-  # In order to keep using public transport As a customer I want to add money to my card
   describe '#top_up' do
 
+    # In order to keep using public transport As a customer I want to add money to my card
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it 'can top up the balance' do
@@ -28,7 +28,17 @@ describe Oystercard do
       expect { subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
     end
 
-
   end
 
+  describe '#deduct' do
+
+    # In order to pay for my journey As a customer I need my fare deducted from my card
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+
+    it "deducts an amount from the balance" do
+      subject.top_up(20)
+      expect{ subject.deduct 3 }.to change{ subject.balance }.by -3
+    end
+
+  end
 end
