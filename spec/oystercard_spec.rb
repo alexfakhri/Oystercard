1beac16eee1ade5182b0109fe47f0f93e03b9760
alexfakhri <alexfakhri@me.com>
2016-07-29 12:44:02 +0100

    added test and method for toping up oyster card, 1 test commented out, just another way of writing the topup test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,9 +2,25 @@ require 'oystercard'
 
 describe Oystercard do
 
+  # In order to use public transport As a customer I want money on my card
   it 'has a balance of zero' do
     expect(subject.balance).to eq 0
   end
 
+  # In order to keep using public transport As a customer I want to add money to my card
+  describe '#top_up' do
+
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can top up the balance' do
+      expect { subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+
+    # it 'can be topped up' do
+    #   subject.top_up(1)
+    #   expect(subject.balance).to eq 1
+    # end
+
+  end
 
 end
