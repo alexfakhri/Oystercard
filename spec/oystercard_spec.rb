f3f180f0cf35f1884ec0cbb0c14b8c1f930f1234
alexfakhri <alexfakhri@me.com>
2016-07-29 13:25:15 +0100

    added test and method for maximum balance, using a constant to set the maximum balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -21,6 +21,14 @@ describe Oystercard do
     #   expect(subject.balance).to eq 1
     # end
 
+    # In order to protect my money from theft or loss As a customer I want a maximum limit (of £90) on my card
+    it 'raises an error if the maximum balance in exceeded' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(maximum_balance)
+      expect { subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+    end
+
+
   end
 
 end
