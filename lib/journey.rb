7eefcb1b447b45e81a395569c5750df1c29b4600
alexfakhri <alexfakhri@me.com>
2016-08-01 22:07:07 +0100

    Added journey class and added first test

---
+++ lib/journey.rb
@@ -0,0 +1,7 @@
+class Journey
+
+  def complete?
+    false
+  end
+
+end
---
+++ spec/journey_spec.rb
@@ -0,0 +1,12 @@
+require 'journey'
+
+describe Journey do
+
+  let(:station) { double :station, Zone:1 }
+
+  it 'knows if a journey is not complete' do
+    expect(subject).not_to be_complete
+  end
+
+
+end
