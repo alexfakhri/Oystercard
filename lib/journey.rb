bb1e7acf7e76b3075d431b5d7e4a807dda60343a
alexfakhri <alexfakhri@me.com>
2016-08-01 23:05:17 +0100

    Adding fare calculator for journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,22 +2,28 @@ class Journey
 
   PENALTY_FARE = 6
 
-  attr_reader :entry_station
+  attr_reader :entry_station, :exit_station
 
   def initialize(entry_station: nil)
     @entry_station = entry_station
   end
 
   def complete?
-    false
+   false
   end
 
   def fare
-    PENALTY_FARE
+    return PENALTY_FARE if penalty?
+    1
   end
 
   def exit(station)
+    @exit_station = station
     self
+  end
+
+  def penalty?
+    !exit_station
   end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -8,7 +8,7 @@ describe Journey do
     expect(subject).not_to be_complete
   end
 
-  it 'has a penalty dare by default' do
+  it 'has a penalty fare by default' do
     expect(subject.fare).to eq Journey::PENALTY_FARE
   end
 
@@ -22,6 +22,24 @@ describe Journey do
 
     it 'has an entry station' do
       expect(subject.entry_station).to eq station
+    end
+
+    it 'returns a penalty fare if no exit station given' do
+      expect(subject.fare).to eq Journey::PENALTY_FARE
+    end
+
+    context 'given an exit station' do
+
+      let(:other_station)  { double :other_station }
+
+      before do
+        subject.exit(other_station)
+      end
+
+      it 'calculates a fare' do
+        expect(subject.fare).to eq 1
+      end
+
     end
 
 
